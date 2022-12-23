2e67319d95fb8242ade6570e269e75f67490a9fe
nosamu <71368227+n0samu@users.noreply.github.com>
2022-12-22 18:31:25 -0600

    render: Disable VERTEX_WRITABLE_STORAGE

--- render/wgpu/src/backend.rs
+++ render/wgpu/src/backend.rs
@@ -582,8 +582,7 @@ async fn request_device(
         .request_device(
             &wgpu::DeviceDescriptor {
                 label: None,
-                features: wgpu::Features::DEPTH24PLUS_STENCIL8
-                    | wgpu::Features::VERTEX_WRITABLE_STORAGE,
+                features: wgpu::Features::DEPTH24PLUS_STENCIL8,
                 limits,
             },
             trace_path,
